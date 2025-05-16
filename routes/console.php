057776ebfc4e6a3e4b8855c604f8e39ab251d6b5
angga1207 <63151469+angga1207@users.noreply.github.com>
2025-05-16 09:50:49 +0700

    Update console.php

--- routes/console.php
+++ routes/console.php
@@ -1,8 +1,48 @@
 <?php
 
+use App\Jobs\AccountancyReportLO;
+use App\Jobs\AccountancyReportNeraca;
+use App\Jobs\ProcessRecap;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Queue;
 
-Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote');
+// Schedule commands
+$schedule = app(\Illuminate\Console\Scheduling\Schedule::class);
+
+$schedule->call(function () {
+    $isRunning = Queue::size('process_recap');
+    if ($isRunning == 0) {
+        dispatch(new ProcessRecap(1))->onQueue('process_recap');
+    }
+    // })->everyFiveMinutes();
+})->everyMinute();
+
+$schedule->call(function () {
+    $arrInstance = DB::table('instances')
+        ->get();
+
+    $chunks = $arrInstance->chunk(5);
+    foreach ($chunks as $key => $chunk) {
+        $ids = $chunk->pluck('id')->toArray();
+        $isRunning = Queue::size('accountancy_report_neraca_' . $key);
+        if ($isRunning == 0) {
+            dispatch(new AccountancyReportNeraca(1, 2024, $ids))->onQueue('accountancy_report_neraca_' . $key);
+        }
+    }
+})->everyThirtyMinutes();
+
+$schedule->call(function () {
+    $arrInstance = DB::table('instances')
+        ->get();
+
+    $chunks = $arrInstance->chunk(3);
+    foreach ($chunks as $key => $chunk) {
+        $ids = $chunk->pluck('id')->toArray();
+        $isRunningLo = Queue::size('accountancy_report_lo_' . $key);
+        if ($isRunningLo == 0) {
+            dispatch(new AccountancyReportLO(1, 2024, $ids))->onQueue('accountancy_report_lo_' . $key);
+        }
+    }
+})->hourly();
