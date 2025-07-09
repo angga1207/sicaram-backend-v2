6215226540ea64b86b188c3600cf098a9a8ac77f
angga1207 <63151469+angga1207@users.noreply.github.com>
2025-07-09 14:09:29 +0700

    Update console.php

--- routes/console.php
+++ routes/console.php
@@ -19,32 +19,32 @@ $schedule->call(function () {
     // })->everyFiveMinutes();
 })->everyMinute();
 
-$schedule->call(function () {
-    $arrInstance = DB::table('instances')
-        ->get();
+// $schedule->call(function () {
+//     $arrInstance = DB::table('instances')
+//         ->get();
 
-    $chunks = $arrInstance->chunk(5);
-    foreach ($chunks as $key => $chunk) {
-        $ids = $chunk->pluck('id')->toArray();
-        $isRunning = Queue::size('accountancy_report_neraca_' . $key);
-        if ($isRunning == 0) {
-            dispatch(new AccountancyReportNeraca(1, 2024, $ids))->onQueue('accountancy_report_neraca_' . $key);
-        }
-    }
-// })->everyThirtyMinutes();
-})->hourlyAt(45);
+//     $chunks = $arrInstance->chunk(5);
+//     foreach ($chunks as $key => $chunk) {
+//         $ids = $chunk->pluck('id')->toArray();
+//         $isRunning = Queue::size('accountancy_report_neraca_' . $key);
+//         if ($isRunning == 0) {
+//             dispatch(new AccountancyReportNeraca(1, 2024, $ids))->onQueue('accountancy_report_neraca_' . $key);
+//         }
+//     }
+// // })->everyThirtyMinutes();
+// })->hourlyAt(45);
 
-$schedule->call(function () {
-    $arrInstance = DB::table('instances')
-        ->get();
+// $schedule->call(function () {
+//     $arrInstance = DB::table('instances')
+//         ->get();
 
-    $chunks = $arrInstance->chunk(3);
-    foreach ($chunks as $key => $chunk) {
-        $ids = $chunk->pluck('id')->toArray();
-        $isRunningLo = Queue::size('accountancy_report_lo_' . $key);
-        if ($isRunningLo == 0) {
-            dispatch(new AccountancyReportLO(1, 2024, $ids))->onQueue('accountancy_report_lo_' . $key);
-        }
-    }
-// })->hourly();
-})->hourlyAt(15);
+//     $chunks = $arrInstance->chunk(3);
+//     foreach ($chunks as $key => $chunk) {
+//         $ids = $chunk->pluck('id')->toArray();
+//         $isRunningLo = Queue::size('accountancy_report_lo_' . $key);
+//         if ($isRunningLo == 0) {
+//             dispatch(new AccountancyReportLO(1, 2024, $ids))->onQueue('accountancy_report_lo_' . $key);
+//         }
+//     }
+// // })->hourly();
+// })->hourlyAt(15);
