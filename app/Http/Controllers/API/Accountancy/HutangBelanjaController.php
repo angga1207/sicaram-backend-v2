214e31bfcd93134b8981252ddd6a120c2ce1b449
angga1207 <63151469+angga1207@users.noreply.github.com>
2026-03-04 11:22:04 +0700

    Update HutangBelanjaController.php

--- app/Http/Controllers/API/Accountancy/HutangBelanjaController.php
+++ app/Http/Controllers/API/Accountancy/HutangBelanjaController.php
@@ -111,8 +111,9 @@ class HutangBelanjaController extends Controller
 
                     'beban' => $data->beban,
                     'jangka_pendek' => $data->jangka_pendek,
-                    'total_hutang' => floatval($data->beban ? $data->beban * -1 : 0) + floatval($data->jangka_pendek ? $data->jangka_pendek * -1 : 0),
+                    // 'total_hutang' => floatval($data->beban ? $data->beban : 0) + floatval($data->jangka_pendek ? $data->jangka_pendek : 0),
                     // 'total_hutang' => $data->kewajiban_tidak_terbayar_last_year ? ($data->kewajiban_tidak_terbayar_last_year > 0 ? (($data->kewajiban_tidak_terbayar_last_year - $data->jumlah_pembayaran_hutang) + $data->sisa_hutang) : $data->sisa_hutang) : 0,
+                    'total_hutang' => floatval($data->beban ?? 0) + floatval($data->jangka_pendek ?? 0) - floatval($data->jumlah_pembayaran_hutang ?? 0),
                     'created_at' => $data->created_at,
                     'updated_at' => $data->updated_at
                 ];
